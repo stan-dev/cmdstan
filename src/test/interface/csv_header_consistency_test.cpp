6aedffce7e9c1200d490df9a41617f8679dfd865
Daniel Lee <bearlee@alum.mit.edu>
2014-02-20 11:11:40 -0500

    test: moved stan's issue91 and issue109 tests to src/test/interface/print_uninitialized_test.cpp and csv_header_consistency_test.cpp

---
+++ src/test/interface/csv_header_consistency_test.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include <stan/gm/error_codes.hpp>
+#include <test/utility.hpp>
+#include <stan/mcmc/chains.hpp>
+
+TEST(gm,csv_header_consistency) {
+  // from stan-dev/stan issue #109
+  std::vector<std::string> model_path;
+  model_path.push_back("src");
+  model_path.push_back("test");
+  model_path.push_back("test-models");
+  model_path.push_back("csv_header_consistency");
+
+  std::string path = convert_model_path(model_path);
+  std::string samples = path + ".csv";
+
+  std::string command
+    = path
+    + " sample num_warmup=0 num_samples=1"
+    + " output file=" + samples;
+
+  run_command_output out = run_command(command);
+  EXPECT_EQ(int(stan::gm::error_codes::OK), out.err_code);
+  EXPECT_FALSE(out.hasError);
+
+  std::ifstream ifstream;
+  ifstream.open(samples.c_str());
+  stan::mcmc::chains<> chains(stan::io::stan_csv_reader::parse(ifstream));
+  ifstream.close();
+  
+  EXPECT_EQ(1, chains.num_samples());
+  EXPECT_FLOAT_EQ(1, chains.samples("z[1,1]")(0));
+  EXPECT_FLOAT_EQ(2, chains.samples("z[1,2]")(0));
+  EXPECT_FLOAT_EQ(3, chains.samples("z[2,1]")(0));
+  EXPECT_FLOAT_EQ(4, chains.samples("z[2,2]")(0));
+  EXPECT_FLOAT_EQ(1, chains.samples("z_mat[1,1]")(0));
+  EXPECT_FLOAT_EQ(2, chains.samples("z_mat[1,2]")(0));
+  EXPECT_FLOAT_EQ(3, chains.samples("z_mat[2,1]")(0));
+  EXPECT_FLOAT_EQ(4, chains.samples("z_mat[2,2]")(0));
+}
---
+++ src/test/interface/print_uninitialized_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <stan/gm/error_codes.hpp>
+#include <test/utility.hpp>
+
+TEST(gm,print_uninitialized) {
+  // This was stan-dev/stan issue #91
+  std::vector<std::string> model_path;
+  model_path.push_back("src");
+  model_path.push_back("test");
+  model_path.push_back("test-models");
+  model_path.push_back("print_uninitialized");
+
+  std::string command 
+    = convert_model_path(model_path)
+    + " sample num_warmup=0 num_samples=0"
+    + " output file=" + convert_model_path(model_path) + ".csv";
+  
+  run_command_output out = run_command(command);
+  EXPECT_EQ(int(stan::gm::error_codes::OK), out.err_code);
+  EXPECT_FALSE(out.hasError);
+}
