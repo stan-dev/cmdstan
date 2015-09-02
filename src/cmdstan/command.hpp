48f90b192673a14ca5ccbc428a8e204efbd31230
Daniel Lee <bearlee@alum.mit.edu>
2015-09-01 23:50:06 -0400

    removing filtered_values from stan

--- src/cmdstan/command.hpp
+++ src/cmdstan/command.hpp
@@ -117,7 +117,6 @@
 #include <stan/interface_callbacks/interrupt/noop.hpp>
 #include <stan/interface_callbacks/var_context_factory/dump_factory.hpp>
 #include <stan/interface_callbacks/writer/csv.hpp>
-#include <stan/interface_callbacks/writer/filtered_values.hpp>
 #include <stan/interface_callbacks/writer/messages.hpp>
 #include <stan/interface_callbacks/writer/noop.hpp>
 #include <stan/interface_callbacks/writer/base_writer.hpp>
