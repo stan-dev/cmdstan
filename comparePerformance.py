9d300de9e3a4cc762d33abcc8ad8e76ae5edbdb5
Dan Luu <danluu@gmail.com>
2018-03-24 21:30:08 -0400

    Add name of each benchmark to output
    This is still a hack, but this makes it easier to figure out if
    something weird is happening.

--- comparePerformance.py
+++ comparePerformance.py
@@ -7,6 +7,13 @@ def geo_mean(iterable):
     a = np.log(iterable)
     return np.exp(a.sum()/len(a))
 
+def get_names(csv):
+    names = []
+    with open(csv) as f:
+        for line in f:
+            names.append(line.split(",")[0])
+    return names
+
 def get_times(csv):
     times = []
     with open(csv) as f:
@@ -18,5 +25,13 @@ if __name__ == "__main__":
     csv1 = sys.argv[1]
     csv2 = sys.argv[2]
     ratios = [t1/t2 for t1, t2 in zip(get_times(csv1), get_times(csv2))]
-    print(ratios)
+    names1 = get_names(csv1)
+    names2 = get_names(csv2)
+
+    assert(len(names1) == len(names2))
+    assert(len(ratios) == len(names1))
+    for i in range(len(names1)):
+        assert(names1[i] == names2[i])
+        print(names1[i], ratios[i])
+
     print(geo_mean(ratios))
