c6b268c5584b368353b9ea3b03dec0b6f3f11af7
J. J. Ramsey <jjramsey@pobox.com>
2018-10-07 20:04:51 -0400

    Fixed max treedepth diagnosis

--- src/test/interface/diagnose_test.cpp
+++ src/test/interface/diagnose_test.cpp
@@ -11,7 +11,7 @@ TEST(CommandDiagnose, corr_gauss) {
   std::string path_separator;
   path_separator.push_back(get_path_separator());
   std::string command = "bin" + path_separator + "diagnose";
-  std::string csv_file 
+  std::string csv_file
     =   "src" + path_separator
       + "test" + path_separator
       + "interface" + path_separator
@@ -19,14 +19,60 @@ TEST(CommandDiagnose, corr_gauss) {
       + "corr_gauss_output.csv";
 
   run_command_output out = run_command(command + " " + csv_file);
-  
+
   ASSERT_FALSE(out.hasError)
     << "\"" << out.command << "\" quit with an error";
-  
+
   std::ifstream expected_output("src/test/interface/example_output/corr_gauss.nom");
   std::stringstream ss;
   ss << expected_output.rdbuf();
-  
+
+  EXPECT_EQ(ss.str(), out.output);
+}
+
+TEST(CommandDiagnose, corr_gauss_depth8) {
+  std::string path_separator;
+  path_separator.push_back(get_path_separator());
+  std::string command = "bin" + path_separator + "diagnose";
+  std::string csv_file
+    =   "src" + path_separator
+      + "test" + path_separator
+      + "interface" + path_separator
+      + "example_output" + path_separator
+      + "corr_gauss_output_depth8.csv";
+
+  run_command_output out = run_command(command + " " + csv_file);
+
+  ASSERT_FALSE(out.hasError)
+    << "\"" << out.command << "\" quit with an error";
+
+  std::ifstream expected_output("src/test/interface/example_output/corr_gauss_depth8.nom");
+  std::stringstream ss;
+  ss << expected_output.rdbuf();
+
+  EXPECT_EQ(ss.str(), out.output);
+}
+
+TEST(CommandDiagnose, corr_gauss_depth15) {
+  std::string path_separator;
+  path_separator.push_back(get_path_separator());
+  std::string command = "bin" + path_separator + "diagnose";
+  std::string csv_file
+    =   "src" + path_separator
+      + "test" + path_separator
+      + "interface" + path_separator
+      + "example_output" + path_separator
+      + "corr_gauss_output_depth15.csv";
+
+  run_command_output out = run_command(command + " " + csv_file);
+
+  ASSERT_FALSE(out.hasError)
+    << "\"" << out.command << "\" quit with an error";
+
+  std::ifstream expected_output("src/test/interface/example_output/corr_gauss_depth15.nom");
+  std::stringstream ss;
+  ss << expected_output.rdbuf();
+
   EXPECT_EQ(ss.str(), out.output);
 }
 
@@ -40,16 +86,16 @@ TEST(CommandDiagnose, eight_schools) {
   + "interface" + path_separator
   + "example_output" + path_separator
   + "eight_schools_output.csv";
-  
+
   run_command_output out = run_command(command + " " + csv_file);
-  
+
   ASSERT_FALSE(out.hasError)
   << "\"" << out.command << "\" quit with an error";
-  
+
   std::ifstream expected_output("src/test/interface/example_output/eight_schools.nom");
   std::stringstream ss;
   ss << expected_output.rdbuf();
-  
+
   EXPECT_EQ(ss.str(), out.output);
 }
 
@@ -63,15 +109,15 @@ TEST(CommandDiagnose, mix) {
   + "interface" + path_separator
   + "example_output" + path_separator
   + "mix_output.*";
-  
+
   run_command_output out = run_command(command + " " + csv_file);
-  
+
   ASSERT_FALSE(out.hasError)
   << "\"" << out.command << "\" quit with an error";
-  
+
   std::ifstream expected_output("src/test/interface/example_output/mix.nom");
   std::stringstream ss;
   ss << expected_output.rdbuf();
-  
+
   EXPECT_EQ(ss.str(), out.output);
 }
