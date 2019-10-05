608027aac893fc7ae61fe6d2f9ce66eeee15ffc0
Rok <rok.cesnovar@fri.uni-lj.si>
2019-10-05 20:43:55 +0200

    use sed instead of grep and cut

--- install-tbb.bat
+++ install-tbb.bat
@@ -1,6 +1,6 @@
 :: this batch script assumes that you have the basic GNU tools (installed with RTools)
 @ECHO off
-reg query HKEY_CURRENT_USER\Environment /v path | grep -o "REG_SZ.*" | cut -d" " -f5 > temp.txt
+reg query HKEY_CURRENT_USER\Environment /v path | sed -n 's/.*REG_SZ[ ]*//p' > temp.txt
 set /p VAR=<temp.txt
-SETX Path %~dp0\lib\tbb;%VAR%
+SETX Path %~dp0lib\tbb;%VAR%
 rm temp.txt
