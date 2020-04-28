e12b98db4f4c5c1c29cb3186e027b83465fe46e6
Steve Bronder <stevo15025@gmail.com>
2020-04-27 21:40:46 -0400

    trying multichain

---
+++ src/cmdstan/arguments/arg_chains.hpp
@@ -0,0 +1,20 @@
+#ifndef CMDSTAN_ARGUMENTS_ARG_CHAINS_HPP
+#define CMDSTAN_ARGUMENTS_ARG_CHAINS_HPP
+
+#include <cmdstan/arguments/arg_num_chains.hpp>
+
+namespace cmdstan {
+
+  class arg_chains: public categorical_argument {
+  public:
+    arg_chains() {
+      _name = "chains";
+      _description = "Number of Chains";
+
+      _subarguments.push_back(new arg_num_chains());
+
+    }
+  };
+
+}
+#endif
---
+++ src/cmdstan/arguments/arg_num_chains.hpp
@@ -0,0 +1,22 @@
+#ifndef CMDSTAN_ARGUMENTS_ARG_NUM_CHAINS_HPP
+#define CMDSTAN_ARGUMENTS_ARG_NUM_CHAINS_HPP
+
+#include <cmdstan/arguments/singleton_argument.hpp>
+
+namespace cmdstan {
+
+  class arg_num_chains: public u_int_argument {
+  public:
+    arg_num_chains(): u_int_argument() {
+      _name = "n_chains";
+      _description = std::string("Number of Chains");
+      _default = "1";
+      _default_value = 1;
+      _value = _default_value;
+    }
+
+    bool is_valid(unsigned int value) { return value > 0; }
+  };
+
+}
+#endif
