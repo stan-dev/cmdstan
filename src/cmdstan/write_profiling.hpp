bb0a1c02cc9feffd547b60d68a258cee3c32476c
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-06-10 22:10:10 +0200

    add total time

--- src/cmdstan/write_profiling.hpp
+++ src/cmdstan/write_profiling.hpp
@@ -7,10 +7,10 @@
 namespace cmdstan {
 
 void write_profiling(stan::callbacks::writer &writer, stan::math::profilers& p) {
-    writer("section,fun_eval,gradient");
+    writer("section,fun_eval_time,gradient_time,total_time");
     std::stringstream msg;
     for (auto const& x : p) {
-        msg << x.first << "," << x.second.fwd_pass_time << "," << x.second.bckwd_pass_time << std::endl;
+        msg << x.first << "," << x.second.fwd_pass_time << "," << x.second.bckwd_pass_time << "," << (x.second.fwd_pass_time + x.second.bckwd_pass_time) << std::endl;
     }
     writer(msg.str());
 }
