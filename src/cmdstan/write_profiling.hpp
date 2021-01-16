fe4ad5d257bef2b1db125b1c7be445ae0632f85f
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-01-16 12:25:20 +0100

    cleanup

--- src/cmdstan/write_profiling.hpp
+++ src/cmdstan/write_profiling.hpp
@@ -14,7 +14,6 @@ void write_profiling(stan::callbacks::writer& writer,
     std::stringstream profile_csv_stream;
     profile_csv_stream << "name,thread_id,time_total,forward_time,reverse_time,chain_stack_total,nochain_stack_total,autodiff_passes,no_autodiff_passes" << std::endl;
     for (it = p.begin(); it != p.end(); it++) {
-        std::cout << it->first.first << std::endl;
         profile_csv_stream 
             // name
             <<  it->first.first << ","
@@ -31,9 +30,9 @@ void write_profiling(stan::callbacks::writer& writer,
             // nochain_stack_total
             << it->second.get_nochain_stack_used() << ","
             // autodiff_passes
-            << it->second.get_num_rev_passes()
+            << it->second.get_num_rev_passes()  << ","
             // no_autodiff_passes
-            << it->second.get_num_no_AD_fwd_passes() << ","
+            << it->second.get_num_no_AD_fwd_passes()
             << std::endl;
     }
     writer(profile_csv_stream.str());
