8f88905d2779130f74678455918eee00dd426e1b
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-01-17 12:24:04 +0100

    add doxygen for write_profiling

--- src/cmdstan/write_profiling.hpp
+++ src/cmdstan/write_profiling.hpp
@@ -8,6 +8,14 @@
 #include <vector>
 
 namespace cmdstan {
+
+/**
+ * Writes the data from the map of profiles in a CSV format
+ * to the supplied writer.
+ * 
+ * @param writer object of a Stan writer class to write to.
+ * @param p reference to the map of profiles
+ */
 void write_profiling(stan::callbacks::writer& writer,
                      stan::math::profile_map& p) {
   stan::math::profile_map::iterator it;
@@ -17,31 +25,22 @@ void write_profiling(stan::callbacks::writer& writer,
                      << std::endl;
   for (it = p.begin(); it != p.end(); it++) {
     profile_csv_stream
-        // name
         << it->first.first
         << ","
-        // thread_id
         << it->first.second
-        << ","  //
-        // time_total
+        << ","
         << (it->second.get_fwd_time() + it->second.get_rev_time())
         << ","
-        // forward_time
         << it->second.get_fwd_time()
         << ","
-        // reverse_time
         << it->second.get_rev_time()
         << ","
-        // chain_stack_total
         << it->second.get_chain_stack_used()
         << ","
-        // nochain_stack_total
         << it->second.get_nochain_stack_used()
         << ","
-        // autodiff_passes
         << it->second.get_num_rev_passes()
         << ","
-        // no_autodiff_passes
         << it->second.get_num_no_AD_fwd_passes() << std::endl;
   }
   writer(profile_csv_stream.str());
