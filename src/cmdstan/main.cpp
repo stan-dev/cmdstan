5213b3a4dee99864506a15651ae792046ccf2faf
Mitzi Morris <mitzi@panix.com>
2021-03-13 22:31:09 -0500

    simplify return codes, model instantiation err msgs go to serr; update unit tests

--- src/cmdstan/main.cpp
+++ src/cmdstan/main.cpp
@@ -1,11 +1,16 @@
 #include <cmdstan/command.hpp>
 #include <stan/services/error_codes.hpp>
 
+
 int main(int argc, const char *argv[]) {
   try {
-    return cmdstan::command(argc, argv);
+    int err_code = cmdstan::command(argc, argv);
+    if (err_code == 0)
+      return cmdstan::return_codes::OK;
+    else
+      return cmdstan::return_codes::NOT_OK;
   } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
-    return stan::services::error_codes::SOFTWARE;
+    return cmdstan::return_codes::NOT_OK;
   }
 }
