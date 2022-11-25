146f5860cd830f546969f4e0f48b0221b9fa87d1
Mitzi Morris <mitzi@panix.com>
2022-11-25 03:46:20 -0500

    unit tests for log_prob constrained_params csv input

--- src/test/interface/log_prob_test.cpp
+++ src/test/interface/log_prob_test.cpp
@@ -14,6 +14,7 @@ class CmdStan : public testing::Test {
  public:
   void SetUp() {
     bern_log_prob_model = {"src", "test", "test-models", "bern_log_prob_model"};
+    bern_gq_model = {"src", "test", "test-models", "bern_gq_model"};
     bern_data = {"src", "test", "test-models", "bern.data.json"};
     bern_unconstrained_params_rdump
         = {"src", "test", "test-models", "bern_unconstrained_params.R"};
@@ -23,6 +24,8 @@ class CmdStan : public testing::Test {
         = {"src", "test", "test-models", "bern_unconstrained_params.json"};
     bern_constrained_params_json
         = {"src", "test", "test-models", "bern_constrained_params.json"};
+    bern_constrained_params_csv
+        = {"src", "test", "test-models", "bern_fitted_params.csv"};
     bern_unconstrained_params_short = {"src", "test", "test-models",
                                        "bern_unconstrained_params_short.json"};
     bern_constrained_params_short
@@ -31,11 +34,13 @@ class CmdStan : public testing::Test {
     test_output = {"test", "output.csv"};
   }
   std::vector<std::string> bern_log_prob_model;
+  std::vector<std::string> bern_gq_model;
   std::vector<std::string> bern_data;
   std::vector<std::string> bern_unconstrained_params_rdump;
   std::vector<std::string> bern_constrained_params_rdump;
   std::vector<std::string> bern_unconstrained_params_json;
   std::vector<std::string> bern_constrained_params_json;
+  std::vector<std::string> bern_constrained_params_csv;
   std::vector<std::string> bern_unconstrained_params_short;
   std::vector<std::string> bern_constrained_params_short;
   std::vector<std::string> dev_null_path;
@@ -59,9 +64,9 @@ TEST_F(CmdStan, log_prob_good_rdump) {
   std::vector<std::string> names;
   boost::split(names, header[0], boost::is_any_of(","),
                boost::token_compress_on);
-  ASSERT_TRUE(values.size() % names.size() == 0);
-  ASSERT_TRUE(names[0].compare(0, 2, std::string("lp")) == 0);
-  ASSERT_TRUE(names[1].compare(0, 2, std::string("g_")) == 0);
+  ASSERT_EQ(values.size() % names.size(), 0);
+  ASSERT_EQ(names[0].compare(0, 2, std::string("lp")), 0);
+  ASSERT_EQ(names[1].compare(0, 2, std::string("g_")), 0);
 
   ss.str(std::string());
   ss << convert_model_path(bern_log_prob_model)
@@ -99,7 +104,7 @@ TEST_F(CmdStan, log_prob_good_json) {
   std::vector<std::string> names;
   boost::split(names, header[0], boost::is_any_of(","),
                boost::token_compress_on);
-  ASSERT_TRUE(values.size() % names.size() == 0);
+  ASSERT_EQ(values.size() % names.size(), 0);
 
   ss.str(std::string());
   ss << convert_model_path(bern_log_prob_model)
@@ -118,6 +123,29 @@ TEST_F(CmdStan, log_prob_good_json) {
   boost::split(names, header[0], boost::is_any_of(","),
                boost::token_compress_on);
   ASSERT_TRUE(values.size() % names.size() == 0);
+}
+
+TEST_F(CmdStan, log_prob_good_csv) {
+  std::stringstream ss;
+  ss << convert_model_path(bern_gq_model)
+     << " data file=" << convert_model_path(bern_data)
+     << " output file=" << convert_model_path(test_output)
+     << " method=log_prob constrained_params="
+     << convert_model_path(bern_constrained_params_csv);
+  std::string cmd = ss.str();
+  run_command_output out = run_command(cmd);
+  ASSERT_FALSE(out.hasError);
+  std::vector<std::string> config;
+  std::vector<std::string> header;
+  std::vector<double> values;
+  parse_sample(convert_model_path(test_output), config, header, values);
+  std::vector<std::string> names;
+  boost::split(names, header[0], boost::is_any_of(","),
+               boost::token_compress_on);
+  ASSERT_EQ(values.size() % names.size(), 0);
+  ASSERT_EQ(names[0].compare(0, 2, std::string("lp")), 0);
+  ASSERT_EQ(names[1].compare(std::string("g_theta")), 0);
+  ASSERT_EQ(values.size(), 2000);  // 1000 draws, 2 cols
 }
 
 TEST_F(CmdStan, log_prob_no_params) {
