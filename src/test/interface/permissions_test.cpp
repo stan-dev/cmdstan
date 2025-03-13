d52478cd559535262969d75903b777d3e874fd8f
Brian Ward <bward@flatironinstitute.org>
2025-03-13 12:07:27 -0400

    Also test multi-chain

--- src/test/interface/permissions_test.cpp
+++ src/test/interface/permissions_test.cpp
@@ -18,3 +18,19 @@ TEST(interface, fails_when_output_is_unwritable) {
   EXPECT_IN_STRING("Permission denied", out.output);
   EXPECT_TRUE(out.hasError);
 }
+
+TEST(interface, fails_when_output_is_unwritable_multi) {
+  temporary_unwritable_file file1(
+      convert_model_path(std::vector{"test", "output_unwritable_1.csv"}));
+  temporary_unwritable_file file2(
+      convert_model_path(std::vector{"test", "output_unwritable_2.csv"}));
+
+  std::string command = convert_model_path(std::vector{
+                            "src", "test", "test-models", "test_model"})
+                        + " sample num_chains=2 output file=" + file1.filename
+                        + "," + file2.filename;
+
+  auto out = run_command(command);
+  EXPECT_IN_STRING("Permission denied", out.output);
+  EXPECT_TRUE(out.hasError);
+}
